97645fcb22de6550b261c9effe1c429958e3a5a8
tylergreen1993 <tyler-green@live.com>
2020-11-29 15:29:43 -0600

    Got good ML score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,11 +10,19 @@ public class GameManager : MonoBehaviour
     public GameObject MiddleBarrier;
     public GameObject[] RoadPieces = new GameObject[2];
     public Text ScoreText;
+    public Color StartScoreColor;
+    public Color EndScoreColor;
     public int Score;
     public bool IsMoving = true;
 
+    public AudioSource Blip;
+    public AudioSource Crash;
+    public AudioSource Rolling;
+
     public float RoadSpeed = 5f;
     public float RoadLength = 250f;
+
+    private bool _scaleScoreText;
 
     void Start()
     {
@@ -49,12 +57,27 @@ public class GameManager : MonoBehaviour
                 RoadPieces[i].transform.position = newRoadPos;
             }
         }
+
+        if (_scaleScoreText)
+        {
+            ScoreText.transform.localScale += Vector3.one * Time.deltaTime;
+            if (ScoreText.transform.localScale.x >= 1.15f)
+            {
+                _scaleScoreText = false;
+            }
+        }
+        else if (ScoreText.transform.localScale.x > 1)
+        {
+            ScoreText.transform.localScale -= Vector3.one * Time.deltaTime;
+        }
     }
 
     public void RestartLevel()
     {
         GameAgent.Instance.GetComponent<GameAgent>().AddReward(-1f);
         GameAgent.Instance.EndEpisode();
+        Rolling.Stop();
+        Crash.Play();
         StartCoroutine(Restart());
     }
 
@@ -63,6 +86,9 @@ public class GameManager : MonoBehaviour
         GameAgent.Instance.GetComponent<GameAgent>().AddReward(1f);
         Score++;
         ScoreText.text = Score.ToString();
+        ScoreText.color = Color.Lerp(StartScoreColor, EndScoreColor, Mathf.Clamp((float)Score / 35, 0, 1));
+        _scaleScoreText = true;
+        Blip.Play();
     }
 
     private void PopulateBarriers(int index)
@@ -100,11 +126,14 @@ public class GameManager : MonoBehaviour
 
     private void StartGame()
     {
+        Rolling.Play();
         Player.GetComponent<MeshRenderer>().enabled = true;
+        Player.transform.position = new Vector3(0, 0.5f, -5.5f);
         RoadPieces[0].transform.position = new Vector3(0, 0, 118.8f);
         RoadPieces[1].transform.position = new Vector3(0, 0, 368.8f);
         PopulateBarriers(0);
         PopulateBarriers(1);
         ScoreText.text = Score.ToString();
+        ScoreText.color = StartScoreColor;
     }
 }
